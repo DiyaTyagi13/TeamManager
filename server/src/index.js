74a80cd8a77498cd1d9c41399c1804860aee0249
DiyaTyagi13 <142670525+DiyaTyagi13@users.noreply.github.com>
2026-05-06 15:33:13 +0530

    routing issue fixed

--- server/src/index.js
+++ server/src/index.js
@@ -32,7 +32,7 @@ app.use('/api/dashboard', dashboardRoutes);
 const clientBuildPath = path.join(__dirname, '../../client/dist');
 app.use(express.static(clientBuildPath));
 
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(clientBuildPath, 'index.html'));
 });
 
